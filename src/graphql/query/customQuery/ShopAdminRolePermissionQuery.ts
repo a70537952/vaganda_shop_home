12a00f854a935844128d202c8eb11881279b66c9
yeekit <a70537952@gmail.com>
2019-10-03 14:17:30 +0800

    generate query

---
+++ src/graphql/query/customQuery/ShopAdminRolePermissionQuery.ts
@@ -0,0 +1,46 @@
+import gql, { disableFragmentWarnings } from 'graphql-tag';
+import { DocumentNode } from 'graphql';
+import { QueryHookOptions, useLazyQuery, useQuery } from '@apollo/react-hooks';
+
+disableFragmentWarnings();
+
+export interface ShopAdminRolePermissionVars {}
+
+export function useShopAdminRolePermissionLazyQuery<TData = any>(
+  fragment: DocumentNode,
+  options?: QueryHookOptions<
+    { shopAdminRolePermission: TData[] },
+    ShopAdminRolePermissionVars
+  >
+) {
+  return useLazyQuery<
+    { shopAdminRolePermission: TData[] },
+    ShopAdminRolePermissionVars
+  >(shopAdminRolePermissionQuery(fragment), options);
+}
+
+export function useShopAdminRolePermissionQuery<TData = any>(
+  fragment: DocumentNode,
+  options?: QueryHookOptions<
+    { shopAdminRolePermission: TData[] },
+    ShopAdminRolePermissionVars
+  >
+) {
+  return useQuery<
+    { shopAdminRolePermission: TData[] },
+    ShopAdminRolePermissionVars
+  >(shopAdminRolePermissionQuery(fragment), options);
+}
+
+export function shopAdminRolePermissionQuery(
+  fragment: DocumentNode
+): DocumentNode {
+  return gql`
+    query ShopAdminRolePermission {
+      shopAdminRolePermission {
+        ...fragment
+      }
+    }
+    ${fragment}
+  `;
+}
